a61345c6024fc2cc0f90cce23be06350d89a622a
Anthony <anlexmatos@users.noreply.github.com>
2015-04-29 06:34:53 -0400

    Revert "Merge branch 'master' of https://github.com/anlexmatos/sb_bandarchiver"
    This reverts commit a266c23b67dd54df1a58879faeb180919201ee14, reversing
    changes made to fedbc22f97dfd1c559063fa4f6e761fb36d7ed68.

--- group_bands.sh
+++ group_bands.sh
@@ -4,7 +4,6 @@
 						
 ####	Initialize variables	####
 				
-<<<<<<< HEAD
 WORKSPACE=$(echo ~/.sb_bandarchiver)				# Set workspace directory
 ARCHIVE_SIZE=10							# Archive size in GiB
 files_per_archive=$(expr $(expr $ARCHIVE_SIZE \* 1024) / 8)	# Calculate archive filecount
@@ -26,32 +25,9 @@ FINAL_BAND=2560;
 
 ########		END INITIALIZATION		 ########
 					
-=======
-WORKSPACE=$(echo ~/.sb_bandarchiver)				#Set workspace directory
-ARCHIVE_SIZE=10							#Archive size in GiB
-files_per_archive=$(expr $(expr $ARCHIVE_SIZE \* 1024) / 8)	#Calculate archive filecount
-
-####	Initialize workspace	####
-
-if [ -e $WORKSPACE ]						
-   then rm $WORKSPACE/counted; rm $WORKSPACE/queues/*; fi	#Wipe workspace dir if it exists
-
-if [ ! -e $WORKSPACE ]						
-   then	mkdir $WORKSPACE; mkdir $WORKSPACE/queues;		#Otherwise, create it
-	echo 'Created workspace at ~/.sb_bandarchiver/';
-fi 
-
-###	TO-DO: User specified band directory (currently the present working directory)	 ###
-###	TO-DO: Find final band automatically	 					 ###
-
-FINAL_BAND=2560;
->>>>>>> ca6fc77f10aa73503158d48cbc71228405ec208c
-
-########		END INITIALIZATION		 ########
-					
 
 
-<<<<<<< HEAD
+
 ####		Create list of all existent bands, in hexadecimal order		####
 
 found_count=0;
@@ -60,17 +36,6 @@ for ((i=0; i<=$FINAL_BAND; i+=1)); do		# Count from zero to (name of last band, 
 	if [ -e $k ]				# Check if the hex in k corresponds to a band
 	then
 		echo $k >> $WORKSPACE/counted	# If so, add to the file list: 'counted'
-=======
-
-####		Create list of all existent bands, in hexadecimal order		####
-
-found_count=0;
-for ((i=0; i<=$FINAL_BAND; i+=1)); do		#Count from zero to (name of last band, as a decimal)
-	k=$(printf "%x\n" $i);			#Convert i to hexadecimal, store in k
-	if [ -e $k ]				#Check if the hex in k corresponds to a band
-	then
-		echo $k >> $WORKSPACE/counted	#If so, add to the file list: 'counted'
->>>>>>> ca6fc77f10aa73503158d48cbc71228405ec208c
 		let "found_count += 1";
 	fi
 done
